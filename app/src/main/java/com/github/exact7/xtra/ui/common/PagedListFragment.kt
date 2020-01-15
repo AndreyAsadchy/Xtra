9f63f2a7ab422fe71bad9e458f8bdda9c7c1a95f
Andrey <samper475@gmail.com>
2020-01-16 02:49:22 +0300

    updates

--- app/src/main/java/com/github/exact7/xtra/ui/common/PagedListFragment.kt
+++ app/src/main/java/com/github/exact7/xtra/ui/common/PagedListFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import com.crashlytics.android.Crashlytics
 import com.github.exact7.xtra.repository.LoadingState
 import com.github.exact7.xtra.util.gone
 import kotlinx.android.synthetic.main.common_recycler_view_layout.*
@@ -24,8 +25,12 @@ abstract class PagedListFragment<T, VM : PagedListViewModel<T>, Adapter : BasePa
                     unregisterAdapterDataObserver(this)
                     registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
                         override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                            if (positionStart == 0) {
-                                recyclerView?.scrollToPosition(0)
+                            try {
+                                if (positionStart == 0) {
+                                    recyclerView?.scrollToPosition(0)
+                                }
+                            } catch (e: Exception) {
+                                Crashlytics.logException(e)
                             }
                         }
                     })
